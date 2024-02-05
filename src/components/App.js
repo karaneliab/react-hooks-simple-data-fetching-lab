ef75fa45d9891d85f133167b66ead58a8e5c5b25
karaneliab <eliabkaran7884@gmail.com>
2024-02-05 12:47:08 +0300

    done with the test

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,30 @@ import React,{useState,useEffect} from "react";
 
 function DataFetching(){
 const [display,setDisplay] = useState(null)
+const[loading,setLoading]= useState(true)
 
 useEffect(() => {
+    setLoading(true)
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
-    .then((data) => setDisplay(data.message))
+    .then((data) =>{
+
+     setDisplay(data.message)
+     setLoading(false)
+    })
 },[])
-  if(!display){
-    return <h3>Wait...</h3>
-  }
+//   if(!display){
+//     return <h3>Wait...</h3>
+//   }
     return(
         <div>
-            <p>Loading...</p>
+             {loading ? (
+        <p>Loading...</p>
+      ) : (
+        display && (
             <img src={display} alt="A Random Dog"/>
+        )
+      )}
         </div>
     )
 }
