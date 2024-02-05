35529aa07c11521f5ac1ab489606d407ed706fef
karaneliab <eliabkaran7884@gmail.com>
2024-02-05 12:36:22 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React,{useState,useEffect} from "react";
+
+function DataFetching(){
+const [display,setDisplay] = useState(null)
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then((data) => setDisplay(data.message))
+},[])
+  if(!display){
+    return <h3>Wait...</h3>
+  }
+    return(
+        <div>
+            <p>Loading...</p>
+            <img src={display} alt="A Random Dog"/>
+        </div>
+    )
+}
+export default DataFetching;
